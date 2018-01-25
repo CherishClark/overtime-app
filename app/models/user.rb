6f4b084851fb2e40c6bbdadc1266e99a38d67040
CherishClark <cherish.g.clark@gmail.com>
2018-01-25 14:33:12 -0700

    Built out rake task for sending sms messages and STI for employees.

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,8 @@ class User < ActiveRecord::Base
 
   validates_format_of :phone, with: PHONE_REGEX
 
+  
+
   def full_name
   	last_name.upcase + ", " + first_name.upcase
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-@user = User.create(email: "test@test.com",
+@employee = Employee.create(email: "test@test.com",
                     password: "123456", 
                     password_confirmation: "123456", 
                     first_name: "Chad", 
@@ -13,14 +13,14 @@ AdminUser.create(email: "admin@test.com",
                  phone: "4807711125")
 
 
-  AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-  AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
-  AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+  AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 6.days))
+  AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 13.days))
+  AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 20.days))
 
 puts "3 audit logs have been created"
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @employee.id, overtime_request: 2.5)
 end
 
 puts "100 Post have been created"
@@ -29,7 +29,7 @@ puts "100 Post have been created"
 
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", user_id: @user.id, overtime_request: 2.5)
+  Post.create!(date: Date.today, rationale: "#{post} rationale content Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", user_id: @employee.id, overtime_request: 2.5)
 end
 
 puts "100 Posts have been created"
