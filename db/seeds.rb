1819b7af8c5a21622648c76b09b5b25370479703
CherishClark <cherish.g.clark@gmail.com>
2018-01-26 11:10:28 -0700

    updated admin home page styles

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,14 +19,6 @@ AdminUser.create(email: "admin@test.com",
 
 puts "3 audit logs have been created"
 
-100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @employee.id, overtime_request: 2.5)
-end
-
-puts "100 Post have been created"
-
-
-
 
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", user_id: @employee.id, overtime_request: 2.5)
